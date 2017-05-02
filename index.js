bf3857f12e8b9dc11f6e6f91ce5e0c732f02aa4a
Volkan Ozcelik <mozcelik@cisco.com>
2017-05-01 20:40:29 -0700

    bugfix

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ const invert = ( fn ) => ( ...args ) => !fn( ...args );
 
 const box = ( x ) => () => x;
 
-const toFunction = ( x ) => if typeof x === 'function' ? x : box( x );
+const toFunction = ( x ) => typeof x === 'function' ? x : box( x );
 
-const toObject = ( x ) => if typeof x === 'object' ? x : { __ref: x };
+const toObject = ( x ) => typeof x === 'object' ? x : { __ref: x };
 
 module.exports = {
     box,
